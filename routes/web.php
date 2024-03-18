9b5fd4c252ef07e27c7df824d4273faa5183d824
fsalles2022 <fabiolfs761@gmil.com>
2024-03-18 19:32:56 -0300

    chore: create routes contact - support-index&create&store

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,18 @@
 <?php
 
+use App\Http\Controllers\Admin\{SupportController};
+use App\Http\Controllers\Site\ContactController;
 use Illuminate\Support\Facades\Route;
+
+
+Route::post('/supports', [SupportController::class, 'store'])->name('supports.store');
+
+Route::get('/supports/create', [SupportController::class, 'create'])->name('supports.create');
+
+Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
+
+Route::get('/contact', [ContactController::class, 'index']);
+
 
 Route::get('/', function () {
     return view('welcome');
