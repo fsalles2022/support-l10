34b17df5e9e7bc3d231a914f676ac968838ec7eb
fsalles2022 <fabiolfs761@gmil.com>
2024-03-19 17:04:38 -0300

    chore: Implements cancel buttom

--- resources/views/admin/supports/create.blade.php
+++ resources/views/admin/supports/create.blade.php
@@ -1,9 +1,10 @@
-<h1> Novas Dúvidas?</h1>
+<h1>Nos informe no que podemos ajudar?</h1>
 
 <form action="{{  route('supports.store') }}" method="POST">
-{{-- <input type="text" value="{{ csrf_token() }}" name="token"> --}}
 @csrf
     <input type="text" placeholder="Assunto" name="subject">
     <textarea name="body" cols="35" rows="6" placeholder="Descrição"></textarea>
     <button type="submit"> Enviar</button>
 </form>
+<a href="{{  route('supports.index') }}">Cancelar</a>
+
