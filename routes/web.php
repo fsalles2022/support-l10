78caa9635bd593b29b159a6a88540dc6d3a3debf
fsalles2022 <fabiolfs761@gmil.com>
2024-03-19 17:12:28 -0300

    chore: Implements route show

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,14 @@ use App\Http\Controllers\Site\ContactController;
 use Illuminate\Support\Facades\Route;
 
 
+
+Route::get('/admin.supports/create', [SupportController::class, 'create'])->name('supports.create');
+
+Route::get('/supports/{id}', [SupportController::class, 'show'])->name('supports.show');
+
 Route::post('/supports', [SupportController::class, 'store'])->name('supports.store');
 
-Route::get('/supports/create', [SupportController::class, 'create'])->name('supports.create');
+
 
 Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
 
