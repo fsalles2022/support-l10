4973e14f670b7fd4b40b73c3c12f51d5d82e001c
Fabio Salles <fabiolfs761@gmail.com>
2024-08-16 12:13:21 -0300

    feat - Adding supportService layer and using it in functions

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -5,13 +5,19 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreUpdateSupportRequest;
 use App\Models\Support;
+use App\Services\SupportService;
 use Illuminate\Http\Request;
 
 class SupportController extends Controller
 {
-    public function index(Support $support)
+    public function __construct(
+        protected SupportService $supportService
+    ) {}
+
+    public function index(Request $request)
     {
-        $supports = support::all();
+        // $supports = support::all();
+        $supports = $this->supportService->getAll($request->filter);
         return view('admin.supports.index', compact('supports'));
     }
 
@@ -20,7 +26,7 @@ class SupportController extends Controller
         //Support::find($id);
         //Support::where('id, '$id')->first();
         //Support::where('id', '=', '$id')->first();
-        if (!$support = Support::find($id)) {
+        if (!$support = $this->supportService->findOne($id)) {
             return back();
         }
         // dd($support->subject);
@@ -37,16 +43,17 @@ class SupportController extends Controller
     {
 
         $data = $request->validated();
-        $data['status'] = 'A';
+        $data['status'] = 'a';
 
         $support->create($data);
 
         return redirect()->route('supports.index');
     }
 
-    public function edit(Support $support, string|int $id)
+    public function edit(string $id)
     {
-        if (!$support = $support->where('id', $id)->first()) {
+        // if (!$support = $support->where('id', $id)->first()) {
+        if (!$support = $this->supportService->findOne($id)) {
             return back();
         }
         return view('admin.supports.edit', compact('support'));
@@ -62,19 +69,15 @@ class SupportController extends Controller
         //$support->subject = $request->subject;
         //$support->body = $request->body;
         //$support->save();
-    //     $support->update($request->only(['subject', 'body' ])
-    // );
+        //     $support->update($request->only(['subject', 'body' ])
+        // );
         $support->update($request->validated());
         return redirect()->route('supports.index');
     }
 
-    public function destroy(string|int $id)
+    public function destroy(int $id)
     {
-        if (!$support = Support::find($id)) {
-            return back();
-        }
-
-        $support->delete();
+        $this->supportService->delete($id);
 
         return redirect()->route('supports.index');
     }
