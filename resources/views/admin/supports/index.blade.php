5f88699cb66e2d3e11a3a5d3d2ef93c3fd20e6fd
Fabio Salles <fabiolfs761@gmail.com>
2024-08-27 12:38:20 -0300

    chore - Changinge object to Array

--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -25,19 +25,19 @@
             @foreach ($supports as $support)
                 <tr>
 
-                    <td style="color:blue;">{{ $support->subject }}-</td>
-                    <td style="color:red;">{{ $support->body }}</td>
-                    <td style="color:green;">{{ $support->status }}-</td>
-                    <td>{{ $support->id }}</td>
+                    <td style="color:blue;">{{ $support['subject'] }}-</td>
+                    <td style="color:red;">{{ $support['body'] }}</td>
+                    <td style="color:green;">{{ $support['status'] }}-</td>
+                    <td>{{ $support['id'] }}</td>
                     <td>
-                        <a href="{{ route('supports.show', $support->id) }}"> Verificar</a>
-                        <a href="{{ route('supports.edit', $support->id) }}"> Editar</a>
+                        <a href="{{ route('supports.show', $support['id']) }}"> Verificar</a>
+                        <a href="{{ route('supports.edit', $support['id']) }}"> Editar</a>
                     </td>
                 </tr>
             @endforeach
         </tbody>
     </table>
-    
+
 
 </body>
 
