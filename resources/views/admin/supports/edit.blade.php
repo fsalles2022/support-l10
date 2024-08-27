509bcd4508633c3fc99ad9ff738a97dd6208353d
Fabio Salles <fabiolfs761@gmail.com>
2024-08-27 12:36:26 -0300

    chore - Created x-alert and include partials form

--- resources/views/admin/supports/edit.blade.php
+++ resources/views/admin/supports/edit.blade.php
@@ -1,16 +1,11 @@
 <h1>Nos informe no que podemos ajudar?{{ $support->id }}</h1>
 
-@if ($errors->any())
-    @foreach ($errors->all() as $error)
-        {{ $error }}
-    @endforeach
-@endif
+<x-alert />
 
 <form action="{{ route('supports.update', $support->id) }}" method="POST">
-    @csrf
     @method('PUT')
-    <input type="text" placeholder="Assunto" name="subject" value="{{ $support->subject }}">
-    <textarea name="body" cols="35" rows="6" placeholder="Descrição">{{ $support->body }}</textarea>
-    <button type="submit"> Enviar</button>
+    @include('admin.supports.partials.form', [
+        'support' => $support,
+    ])
 </form>
 <a href="{{ route('supports.index') }}">Cancelar</a>
