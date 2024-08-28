357c34da5a0ffbf19c4b6104e4cff2f04eac632f
fsalles2022 <fabiolfs761@gmail.com>
2024-08-28 13:54:40 -0300

    chore - Created Functions for Paging

--- app/Repositories/PaginationInterface.php
+++ app/Repositories/PaginationInterface.php
@@ -2,4 +2,15 @@
 
 namespace App\Repositories;
 
-interface PaginationInterface {}
+interface PaginationInterface
+{
+    // @return StdClass[]
+
+    public function items(): array;
+    public function total(): int;
+    public function isFirstPage(): bool;
+    public function isLastPage(): bool;
+    public function currentPage(): int;
+    public function getNumberNextPage(): int;
+    public function getNumberPreviousPage(): int;
+}
