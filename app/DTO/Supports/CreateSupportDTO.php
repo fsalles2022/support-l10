92ed9b5f3c4cf244187f49064043bd64004a3107
Fabio Salles <fabiolfs761@gmail.com>
2024-09-05 12:34:43 -0300

    chore - Inserting files into Supports folders

---
+++ app/DTO/Supports/CreateSupportDTO.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\DTO\Supports;
+
+use App\Enums\SupportStatus;
+use App\Http\Requests\StoreUpdateSupportRequest;
+
+class CreateSupportDTO
+{
+    public function __construct(
+        public string $subject,
+        public SupportStatus $status,
+        public string $body,
+
+    ) {}
+
+    public static function makeFromRequest(StoreUpdateSupportRequest $request): self
+    {
+        return new self(
+            $request->subject,
+            SupportStatus::Ativo,
+            $request->body,
+        );
+    }
+}
---
+++ app/DTO/Supports/UpdateSupportDTO.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\DTO\Supports;
+
+use App\Enums\SupportStatus;
+use App\Http\Requests\StoreUpdateSupportRequest;
+
+class UpdateSupportDTO
+{
+    public function __construct(
+        public string $id,
+        public string $subject,
+        public SupportStatus $status,
+        public string $body,
+
+    ) {}
+
+    public static function makeFromRequest(StoreUpdateSupportRequest $request): self
+    {
+        return new self(
+            $request->id,
+            $request->subject,
+            SupportStatus::Ativo,
+            $request->body,
+        );
+    }
+}
