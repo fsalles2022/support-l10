7c19ade32b9af027c613bea98967a59b6a87c962
Fabio Salles <fabiolfs761@gmail.com>
2024-09-06 14:56:59 -0300

    feat - Creatint file create.blade partials

--- resources/views/admin/supports/create.blade.php
+++ resources/views/admin/supports/create.blade.php
@@ -5,16 +5,9 @@
     @include('admin.supports.partials.nav')
 @endsection
 
-<div class="container">
-    <h1>Nos informe no que podemos ajudar?</h1>
-
-    <x-alert />
-
-    <form action="{{ route('supports.store') }}" method="POST">
-        @include('admin.supports.partials.form');
-    </form>
-    <a href="{{ route('supports.index') }}">Cancelar</a>
-</div>
+@section('content')
+    @include('admin.supports.partials.create')
+@endsection
 
 @section('content-footer')
     @include('admin.supports.partials.footer')
