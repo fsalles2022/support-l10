397db93adb91f79f3dac9f9b01ea260bc0644533
Fabio Salles <fabiolfs761@gmail.com>
2024-09-10 18:05:22 -0300

    feat - Create file form_edit

--- resources/views/admin/supports/partials/form_edit.blade.php
+++ resources/views/admin/supports/partials/form_edit.blade.php
@@ -2,7 +2,7 @@
     <!-- Exibe o ID do chamado -->
     <div class="mx-auto" style="max-width: 600px;">
         <h3 class="text-center text-white bg-primary p-3 rounded-3 shadow-sm">
-            EDITAR CHAMADO ID: {{ $support->id }}
+            CHAMADO ID: {{ $support->id }}
         </h3>
 
         <x-alert />
@@ -11,10 +11,37 @@
             @csrf
             @method('PUT')
 
-            <!-- Inclui o formulário parcial -->
-            @include('admin.supports.partials.form', ['support' => $support])
+            @csrf
+            <div class="container my-5" style="max-width: 600px;">
+                <!-- Assunto -->
+                <div class="mb-4">
+                    <label class="form-label" for="subject">Assunto</label>
+                    <input type="text" id="subject" class="form-control form-control-lg" name="subject"
+                        value="{{ $support->subject ?? old('subject') }}" required />
+                    <!-- Mensagens de erro -->
+                    @error('subject')
+                        <div class="text-danger mt-2">{{ $message }}</div>
+                    @enderror
+                </div>
 
+                <!-- Mensagem -->
+                <div class="mb-4">
+                    <label class="form-label" for="body">Mensagem</label>
+                    <textarea class="form-control form-control-lg" id="body" name="body" required>{{ $support->body ?? old('body') }}</textarea>
+                    <!-- Mensagens de erro -->
+                    @error('body')
+                        <div class="text-danger mt-2">{{ $message }}</div>
+                    @enderror
+                </div>
 
+                <div class="d-flex justify-content-between mt-4">
+                    <button type="submit" class="btn btn-primary btn-lg w-100 me-2">Editar Chamado</button>
+                    <a href="{{ route('supports.index') }}" class="btn btn-secondary btn-lg w-100 ms-2">Cancelar</a>
+                </div>
+            </div>
         </form>
     </div>
 </div>
+<script>
+    CKEDITOR.replace('body');
+</script>
