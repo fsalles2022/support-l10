29f406b21bd2dd83f11766a6e63b777c4c466498
Fabio Salles <fabiolfs761@gmail.com>
2024-09-11 16:41:55 -0300

    feat - Implemented UUID and foreign

--- database/migrations/2024_03_18_182828_create_supports_table.php
+++ database/migrations/2024_03_18_182828_create_supports_table.php
@@ -13,11 +13,15 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('supports', function (Blueprint $table) {
-            $table->id();
-            $table->string('subject');
+            $table->uuid('id')->primary();
+            $table->uuid('user_id')->index();
             $table->enum('status', array_column(SupportStatus::cases(), 'name'));
             $table->text('body');
             $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
         });
     }
 
