e4b5e94f7b289cf70692e66a427df30ac1be9998
Fabio Salles <fabiolfs761@gmail.com>
2024-09-12 18:02:24 -0300

    Feat - Instal Breeze and config routes web

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Enums\SupportStatus;
 use App\Http\Controllers\Admin\{SupportController};
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Site\ContactController;
 use Illuminate\Support\Facades\Route;
 
@@ -28,3 +29,19 @@ Route::get('/contact', [ContactController::class, 'index']);
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
