eda72240abae2459c5255694460c4896326cc4a4
Fabio Salles <fabiolfs761@gmail.com>
2024-09-23 15:39:28 -0300

    Feat - Created midlewre sanctum and routes to auth, logout, login and me

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,18 @@
 use App\Http\Controllers\Api\SupportController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\Auth\AuthController;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
-Route::apiResource('/supports', SupportController::class);
+Route::post('/login', [AuthController::class, 'auth']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::get('/me', [AuthController::class, 'me'])->middleware('auth:sanctum');
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::apiResource('/supports', SupportController::class);
+});
+
+// Route::apiResource('/supports', SupportController::class)->middleware('auth');
