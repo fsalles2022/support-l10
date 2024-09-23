cee82e0b6c4d4bbfa9192c60d5cd7ffdaf6784c1
Fabio Salles <fabiolfs761@gmail.com>
2024-09-23 12:29:48 -0300

    Feat - Improve layout

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -5,8 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('tilte') - {{ config('app.name') }}</title>
-    {{-- @vite('resources/css/style.css')
-    @vite('resources/css/styleLogin.css') --}}
+    @vite('resources/css/style.css')
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link
@@ -16,8 +15,8 @@
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.ckeditor.com/4.20.1/standard/ckeditor.js"></script>
     {{-- <script src="{{ mix('js/app.js') }}" defer></script> --}}
-    <link rel="stylesheet" href="resources/css/style.css">
-    <link rel="stylesheet" href="resources/css/styleLogin.css">
+    {{-- <link rel="stylesheet" href="resources/css/style.css">
+    <link rel="stylesheet" href="resources/css/styleLogin.css"> --}}
 </head>
 
 <body style="background-color: rgb(23, 22, 22)">
--- resources/views/admin/supports/partials/content.blade.php
+++ resources/views/admin/supports/partials/content.blade.php
@@ -1,39 +1,40 @@
-<div class="container fw-semibold">
-    <h1 class="text-center my-2 text-white">
+<div class="container-fluid fw-semibold">
+    <h1 class="text-center my-2 text-white" style="font-size: 2.5rem; text-shadow: 1px 1px 2px rgba(0,0,0,0.5);">
         TradeUp Group Suporte
     </h1>
     <form action="{{ route('supports.index') }}" method="get" class="d-flex mb-4" role="search">
         <input name="filter" value="{{ $filters['filter'] ?? '' }}" class="form-control me-2" type="text"
-            placeholder="Procurar Chamado" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Pesquisar</button>
+            placeholder="Procurar Chamado" aria-label="Search" style="border-radius: 20px;">
+        <button class="btn btn-success" type="submit" style="border-radius: 20px;">Pesquisar</button>
     </form>
 
     <div class="mb-4 text-center">
-        <a href="{{ route('supports.create') }}" class="btn btn-primary me-2">Criar Chamado</a>
-        <a href="{{ route('supports.index') }}" class="btn btn-primary">Chamados</a>
+        <a href="{{ route('supports.create') }}" class="btn"
+            style="background-color: #0056b3; color: white; border-radius: 20px;">Criar Chamado</a>
+        <a href="{{ route('supports.index') }}" class="btn"
+            style="background-color: #0056b3; color: white; border-radius: 20px;">Tickets</a>
     </div>
 
-    <div class="card">
-        <div class="card-header">
-            <h4 class="mb-0">Tabela de Chamados</h4>
+    <div class="card" style="border-radius: 15px; box-shadow: 0 4px 10px rgba(0,0,0,0.1);">
+        <div class="card-header" style="background-color: #343a40; border-radius: 15px 15px 0 0;">
+            <h4 class="mb-0 text-center" style="color: white;">Tabela de Chamados</h4>
         </div>
         <div class="card-body">
-            <p class="card-text text-muted">TradeUp Group</p>
             <div class="table-responsive">
-                <!-- Mensagem de Nenhum Resultado Encontrado -->
                 @if ($noResults)
                     <div class="alert alert-warning text-center" role="alert">
                         Nenhum resultado encontrado para o filtro aplicado.
                     </div>
                 @else
-                    <table class="table table-striped table-hover">
-                        <thead class="table-dark">
+                    <table class="table table-striped" style="border-radius: 10px; overflow: hidden;">
+                        <thead style="background-color: #007bff; color: white;">
                             <tr class="text-center">
                                 <th>Foto</th>
-                                <th>Nome</th>
+                                <th>Nome Usuário</th>
                                 <th>Assunto</th>
                                 <th>Descrição</th>
                                 <th>Data Criação</th>
+                                <th>Modificado</th>
                                 <th>Status</th>
                                 <th>ID</th>
                                 <th>Ações</th>
@@ -41,41 +42,50 @@
                         </thead>
                         <tbody>
                             @foreach ($supports->items() as $support)
-                                <tr>
-                                    <td class="py-1 px-2"> <!-- Diminuindo o padding -->
+                                <tr class="text-center">
+                                    <td class="mx-2">
                                         <img src="https://img.icons8.com/color/36/000000/administrator-male.png"
                                             alt="imagem">
                                     </td>
-                                    <td class="py-1 px-2">Herman Beck</td>
-                                    <td class="py-1 px-2 text-primary">{{ strtoupper($support->subject) }}</td>
-                                    <td class="py-1 px-2 text-success"> {!! strtoupper($support->body) !!}</td>
-                                    <td class="py-1 px-2 text-success text-center">
+                                    <td class="mx-2">{{ $support->name ?? 'N/A' }}</td>
+                                    <td class="mx-2 text-primary">{{ strtoupper($support->subject) }}</td>
+                                    <td class="mx-2 text-success">{!! strtoupper($support->body) !!}</td>
+                                    <td class="mx-2 text-center">
                                         {{ date('d/m/Y H:i', strtotime($support->created_at)) }}</td>
-                                    <td class="py-1 px-2 text-center">
+                                    <td class="mx-2 text-center">
+                                        {{ date('d/m/Y H:i', strtotime($support->updated_at)) }}</td>
+                                    <td class="mx-2 text-center">
                                         <x-status-support :status="$support->status"></x-status-support>
                                     </td>
-                                    <td class="py-1 px-2 text-success text-center">{{ $support->id }}</td>
-                                    <td class="py-1 px-2">
-                                        <a href="{{ route('supports.show', $support->id) }}"
-                                            class="btn btn-info btn-sm">
+                                    <td class="mx-2 text-center">{{ $support->id }}</td>
+
+                                    <td class="mx-2  text-center">
+                                        <a href="{{ route('supports.show', $support->id) }}" class="btn btn-sm"
+                                            style="background-color: #151f20; color: white; border-radius: 20px; margin-right: 5px;">
                                             <i class="fas fa-eye"></i> Verificar
                                         </a>
-                                        <a href="{{ route('supports.edit', $support->id) }}"
-                                            class="btn btn-warning btn-sm">
+                                        <a href="{{ route('supports.edit', $support->id) }}" class="btn btn-sm"
+                                            style="background-color: #baa260; color: white; border-radius: 20px;">
                                             <i class="fas fa-edit"></i> Editar
                                         </a>
                                     </td>
                                 </tr>
                             @endforeach
                         </tbody>
-
                     </table>
                 @endif
-                <div class="pt-1 flex items-center justify-between">
-                    <x-pagination :paginator="$supports" :appends="$filters" />
-                    <h1 class="my-4">Listagem de Chamados - Total: {{ $supports->total() }}</h1>
-                </div>
 
+
+            </div>
+        </div>
+        <div class="card-header mx-2" style="background-color: #343a40; border-radius: 0 0 15px 15px;">
+            <div class="p-1 d-flex justify-content-between align-items-center my-2 rounded"
+                style="background-color: #343a40;">
+                <div>
+                    <x-pagination :paginator="$supports" :appends="$filters" />
+                </div>
+                <h1 class="my-0 fs-6 " style="color: white;">Total de Chamados - Total: {{ $supports->total() }}
+                </h1>
             </div>
         </div>
     </div>
